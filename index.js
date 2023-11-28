4145572738fc479ad3551be8decc962dc3cb025f
vicheath <vicheath.a.s@gmail.com>
2023-11-28 11:23:24 -0500

    third commit

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-for (let i = 0; i < 10; i++) {
-	console.log(i);
-}
+// for (let i = 0; i < 10; i++) {
+// 	console.log(i);
+// }
 
-// Count down from 10 to 1 
-for (let i =10; i > 1; i--){
-    console.log(i)
+// // Count down from 10 to 1 
+// for (let i =10; i > 1; i--){
+//     console.log(i)
+// }
+
+// Output odd mnumbers from 1 to 10
+// for (let i = 0; i  < 10; i++){
+//     if(i % 2 !== 0){
+//         console.log(i)
+//     }
+// }
+
+// Output even number from 1 to 10
+
+for (let i = 2; i < 10; i++){
+    if(i % 1 !==0){
+        console.log(i)
+    }
 }
